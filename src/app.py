767b51943e8c5a417f6e260e632367a82204ebf4
ines5388 <142507801+ines5388@users.noreply.github.com>
2024-04-08 10:13:59 +0000

    Proyecto Jackson Family

--- src/app.py
+++ src/app.py
@@ -1,44 +1,66 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+@app.route('/member', methods=['POST'])
+def add_a_member():
+    try:
+        request_body = request.json
+        member = jackson_family.add_member(request_body)
+        if member is None:
+            return jsonify({'error': 'Member not found'}), 404
+        return jsonify(member), 200
+    except:
+        return jsonify({'error': 'Internal Server Error'}), 500
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_a_member(id):
+    try:
+        member = jackson_family.get_member(id)
+        if member:
+            jackson_family.delete_member(id)
+            return jsonify({'done': True}), 200
+        else:
+            return jsonify({'error': 'Member not found'}), 404
+    except:
+        return jsonify({'error': 'Internal Server Error'}), 500
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member_info(id):
+    try:
+        member = jackson_family.get_member(id)
+        if member is None:
+            return jsonify({'error': 'Member not found'}), 404
+        return jsonify(member), 200
+    except:
+        return jsonify({'error': 'Internal Server Error'}), 500
+
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
-    return jsonify(response_body), 200
-
-# this only runs if `$ python src/app.py` is executed
+def get_members_info():
+    try:
+        members = jackson_family.get_all_members()
+        if members is None:
+            return jsonify({'error': 'Members not found'}), 404
+        return jsonify(members), 200
+    except:
+        return jsonify({'error': 'Internal Server Error'}), 500
+    
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
