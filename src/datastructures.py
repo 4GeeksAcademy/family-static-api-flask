67102060c173c1d4e9ac212a210a8ff9be018974
ines5388 <142507801+ines5388@users.noreply.github.com>
2024-04-09 07:11:24 +0000

    Version Final

--- src/datastructures.py
+++ src/datastructures.py
@@ -7,21 +7,21 @@ class FamilyStructure:
             {
                 'id': self._generateId(),
                 'firt_name': "John",
-                'last_name': "last_name",
+                'last_name': self.last_name,
                 'age': 33,
                 'lucky_numbers': [7, 13, 22]
             },
             {
                 'id': self._generateId(),
                 'first_name':"Jane",
-                'last_name':"last_name",
+                'last_name': self.last_name,
                 'age':35,
                 'lucky_numbers': [10, 14, 3]
             },
             {
                 'id': self._generateId(),
                 'first_name': "Jimmy",
-                'last_name':"last_name",
+                'last_name': self.last_name,
                 'age': 5,
                 'lucky_numbers': [1]
             },
